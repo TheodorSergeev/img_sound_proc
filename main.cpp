1364c6e00516b0af34e7cb08077dccc132be96c3
Fedor Sergeev <theodor.sergeev@yandex.ru>
2022-12-04 14:26:05 +0100

    implement first parser, try linking opencv

--- main.cpp
+++ main.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
-#include <Eigen/Dense>
-//#include "opencv2/core.hpp"
+#include <vector>
+#include <string>
+#include <exception>
+#include "Eigen/Dense"
+//#include <opencv2/opencv.hpp>
+//#include <opencv2/imgcodecs.hpp>
+
+//using cv::imread;
 
 using std::cout;
+using std::string;
+using std::vector;
 using Eigen::MatrixXi;
 using Eigen::MatrixXd;
+
 
 template <typename TInput, typename TOutput>
 class Transform {
@@ -44,6 +53,7 @@ public:
     };
 };
 
+
 class Histogram: public Transform<MatrixXi, MatrixXd> {
 public:
     explicit Histogram() = default;
@@ -60,7 +70,6 @@ public:
 
         for (int i = 0; i < nrows; ++i) {
             for (int j = 0; j < ncols; ++j) {
-                cout << item(i, j) - min_val  << " " << size << "\n";
                 hist(0,item(i, j) - min_val) += 1. / size;
             }
         }
@@ -69,11 +78,75 @@ public:
     };
 };
 
-a
 
-int main() {
+template <typename TInput, typename TOutput>
+class Parser {
+protected:
+    string name;
+
+public:
+    string get_name() {
+        return name;
+    }
+
+    virtual const Transform<TInput, TOutput>* parse(vector <string> arguments) = 0;
+};
+
+class ThresholdingParser: public Parser<MatrixXi, MatrixXi> {
+public:
+    ThresholdingParser() {
+        name = "threshold";
+    }
+
+    const Transform<MatrixXi, MatrixXi>* parse(vector <string> arguments) override {
+        const int n_args = 2;
+
+        if (arguments.size() != n_args)
+            throw std::invalid_argument("Thresholding requires two integer arguments.");
+
+        int thr_min = std::stoi(arguments[0]); // throws an exception if not an int
+        int thr_max = std::stoi(arguments[1]); // throws an exception if not an int
+
+        return new Thresholding(thr_min, thr_max);
+    }
+};
+
+
+int main() { //int argc, char* argv[]) {
     //Matrix<int, -1, 1> img;
-    MatrixXi img(3,3);
+
+    const char* HELP_STR = "--help";
+    const char* HELP_MSG = "asdasd";
+
+    int argc = 4;
+    const char* argv[] = {"path", "threshold", "3", "9"};
+
+    std::array parsers_list = {ThresholdingParser()};
+
+    if (argc == 1) {
+        cout << "Not enough arguments. Use \"./img_sound-proc --help\" to access the options.\n";
+    } else {
+        string opt_str(argv[1]);
+        cout << opt_str << "\n";
+
+        vector<string> arg_vec;
+        for (int i = 2; i < argc; ++i) {
+            arg_vec.emplace_back(argv[i]);
+        }
+
+        if (opt_str == HELP_STR) {
+            cout << HELP_MSG << "\n";
+        }
+
+        for (auto & parser : parsers_list) {
+            if (opt_str == parser.get_name()) {
+                cout << "found " << parser.get_name() << "\n";
+                //pars.parse();
+            }
+        }
+    }
+
+    /*MatrixXi img(3,3);
     img << 1, 2, 3,
            4, 5, 6,
            7, 8, 9;
@@ -85,6 +158,9 @@ int main() {
 
     Histogram hist;
     cout << "Histogram\n" << hist.transform(img);
+
+    string img_path("/data/images/cameraman.tif");*/
+    //cv::Mat image = cv::imread(img_path, cv::IMREAD_COLOR);
 
     return 0;
 }
