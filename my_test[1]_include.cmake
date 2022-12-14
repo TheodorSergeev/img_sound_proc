8f6bdea10980de622e453304abd1b722fbfc3c32
Fedor Sergeev <Theodor.Sergeev@yandex.ru>
2022-12-14 16:05:34 +0100

    add missing opencv files

---
+++ my_test[1]_include.cmake
@@ -0,0 +1,5 @@
+if(EXISTS "/home/fedor/Desktop/img_sound_proc/my_test[1]_tests.cmake")
+  include("/home/fedor/Desktop/img_sound_proc/my_test[1]_tests.cmake")
+else()
+  add_test(my_test_NOT_BUILT my_test_NOT_BUILT)
+endif()
