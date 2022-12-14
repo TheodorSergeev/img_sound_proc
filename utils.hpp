9113004a198673236938c568f6c25472a7e27543
Fedor Sergeev <Theodor.Sergeev@yandex.ru>
2022-12-14 17:26:56 +0100

    comment utils functions

--- utils.hpp
+++ utils.hpp
@@ -13,10 +13,38 @@ using std::string;
 using Eigen::MatrixXi;
 using Eigen::MatrixXd;
 
+/**
+ * @brief Read integer matrix from a file.
+ * Supported formats: image (e.g. png and tiff).
+ * 
+ * @param inp_fname Input filename. 
+ * @return MatrixXi Read matrix.
+ */
 MatrixXi readIntMatrix(const string& inp_fname);
+
+/**
+ * @brief Save integer matrix in a file.
+ * Supported formats: image (e.g. png and tiff).
+ * 
+ * @param out_fname 
+ * @param matrix 
+ */
 void writeIntMatrix(const string& out_fname, const MatrixXi& matrix);
 MatrixXd readFloatMatrix(const string& inp_fname);
 
+/**
+ * @brief Convert opencv matrix to eigen integer matrix.
+ * 
+ * @param image Matrix in the opencv format.
+ * @return MatrixXi Integer matrix in the eigen format.
+ */
 MatrixXi opencv2eigen(const cv::Mat& image);
+
+/**
+ * @brief Convert eigen matrix to opencv matrix.
+ * 
+ * @param mat Integer matrix in the eigen format.
+ * @return cv::Mat Matrix in the opencv format.
+ */
 cv::Mat eigen2opencv(const MatrixXi& mat);
 #endif
